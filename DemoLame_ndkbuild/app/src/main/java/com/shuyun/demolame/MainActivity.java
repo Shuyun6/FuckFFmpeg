5ea305b2aac9075f498ec4d5c44287dc88010258
shuyun <shuyungogogo@hotmail.com>
2020-07-25 19:18:09 +0800

    configurate project and write a simple demo

--- DemoLame_ndkbuild/app/src/main/java/com/shuyun/demolame/MainActivity.java
+++ DemoLame_ndkbuild/app/src/main/java/com/shuyun/demolame/MainActivity.java
@@ -4,27 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+
+    private String pcmFilePath = "sdcard/testcase.wav";
+    private String mp3FilePath = "sdcard/testmp3.mp3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+        tv.setOnClickListener(view ->{
+            new Thread(){
+                @Override
+                public void run() {
+                    super.run();
+                    init(pcmFilePath, mp3FilePath, 44100, 2, 320*1000);
+                    encode();
+                    MainActivity.this.destroy();
+                }
+            }.start();
+            Toast.makeText(getBaseContext(), "encode", Toast.LENGTH_SHORT).show();
+        });
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
     public native String stringFromJNI();
+
+    public native void init(String pcmFilePath, String map3FilePath, int sampleRate, int channels, int bitRate);
+    public native void encode();
+    public native void destroy();
 }
