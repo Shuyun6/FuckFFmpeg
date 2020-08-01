d16e4e8e5a35b5dc307f4599d0843f4c162138e1
Shuyun6 <shuyungogogo@hotmail.com>
2020-08-02 00:13:09 +0800

    fix scripts

--- Script/ffmpeg_full/ffmpeg/build.sh
+++ Script/ffmpeg_full/ffmpeg/build.sh
@@ -1,79 +1,84 @@
 #!/bin/bash
+set -x
 API=24
 CPU=armv7-a
 OUTPUT=/home/shuyun/dev/output
 NDK=/home/shuyun/dev/android-ndk-r21d
 TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
 SYSROOT=$TOOLCHAIN/sysroot
-
+CONFIGURE_FLAGS=
+--disable-static \
+--enable-shared \
+--disable-stripping \
+--disable-ffmpeg \
+--disable-ffplay \
+--disable-ffserver \
+--disable-ffprobe \
+--disable-avdevice \
+--disable-devices \
+--disable-indevs \
+--disable-outdevs \
+--disable-debug \
+--disable-yasm \
+--disable-doc \
+--enable-asm \
+--enable-neon \
+--enable-small \
+--enable-dct \
+--enable-dwt \
+--enable-lsp \
+--enable-mdct \
+--enable-rdft \
+--enable-fft \
+--enable-version3 \
+--enable-small \
+--enable-nonfree \
+--disable-filters \
+--disable-postproc \
+--disable-bsfs \
+--enable-bsf=aac_adtstoasc \
+--enable-bsf=h264_mp4toannexb \
+--disable-encoders \
+--enable-encoder=pcm_s161e \
+--enable-encoder=aac \
+--enable-encoder=libvo_aacenc \
+--disable-decoders \
+--enable-decoder=aac \
+--enable-decoder=mp3 \
+--enable-decoder=pcm-s161e \
+--disable-parsers \
+--enable-parser=aac \
+--disable-muxers \
+--enable-muxer=flv \
+--enable-muxer=wav \
+--enable-muxer=adts \
+--disable-demuxers \
+--enable-demuxer=flv \
+--enable-demuxer=wav \
+--enable-demuxer=aac \
+--disable-protocols \
+--enable-protocol=rtmp \
+--enable-protocol=file \
+--enable-libfdk-aac \
+--enable-libx264 \
+--enable-cross-compile \
+--prefix=$INSTALL_DIR \
+--enable-bsf=aac_adtstoasc \
+--enable-bsf=h264_mp4toannexb \
 
 function build
 {
-  ./configure --prefix=$OUTPUT \
+  ./configure \
+  $CONFIGURE_FLAGS \
+  --prefix=$OUTPUT \
   --target-os=android \
   --arch=arm \
   --cpu=armv7-a \
-  --enable-asm \
-  --enable-neon \
-  --enable-cross-compile \
-  --enable-shared \
-  --disable-ffmpeg \
-  --disable-ffplay \
-  --disable-ffprobe \
-  --disable-avdevice \
-  --disable-devices \
-  --disable-indevs \
-  --disable-outdevs \
-  --disable-debug \
-  --disable-yasm \
-  --disable-doc \
-  --enable-small \
-  --enable-dct \
-  --enable-dwt \
-  --enable-lsp \
-  --enable-mdct \
-  --enable-rdft \
-  --enable-fft \
-  --enable-version3 \
-  --enable-small \
-  --enable-nonfree \
-  --disable-filters \
-  --disable-postproc \
-  --disable-bsfs \
-  --enable-bsf=aac_adtstoasc \
-  --enable-bsf=h264_mp4toannexb \
-  --disable-encoders \
-  --enable-encoder=pcm_s161e \
-  --enable-encoder=aac \
-  --enable-encoder=libvo_aacenc \
-  --disable-decoders \
-  --enable-decoder=aac \
-  --enable-decoder=mp3 \
-  --enable-decoder=pcm-s161e \
-  --disable-parsers \
-  --enable-parser=aac \
-  --disable-muxers \
-  --enable-muxer=flv \
-  --enable-muxer=wav \
-  --enable-muxer=adts \
-  --disable-demuxers \
-  --enable-demuxer=flv \
-  --enable-demuxer=wav \
-  --enable-demuxer=aac \
-  --disable-protocols \
-  --enable-protocol=rtmp \
-  --enable-protocol=file \
-  --enable-libx264 \
-  --enable-libfdk-aac \
-  --prefix=$INSTALL_DIR \
-  --enable-gpl \
-  --enable-bsf=aac_adtstoasc \
-  --enable-bsf=h264_mp4toannexb \
   --sysroot=$SYSROOT \
   --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
   --cross-prefix-clang=$TOOLCHAIN/bin/armv7a-linux-androideabi$API- \
-  --extra-cflags="-fPIC -Ifdk-aac/include -Ix264/include" \
-  --extra-ldflags="-Lfdk-aac/lib -Lx264/lib"
+  --extra-cflags="-fPIC -Ifdk-aac/include -Ix264/include"\
+  --extra-ldflags="-Ifdk-aac/lib -Ix264/lib"
 
   make clean all
   make -j12
